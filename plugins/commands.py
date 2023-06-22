845f4e5959a0885f144396eba72af82d6635d5f3
RAM108MV <137316890+RAM108MV@users.noreply.github.com>
2023-06-22 14:39:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -234,7 +234,7 @@ async def start(client, message):
             return
         except:
             pass
-        return await message.reply(f"📝 ꜰɪʟᴇ ɴᴀᴍᴇ:- {file_name}\n\n🔅 sɪᴢᴇ:- {file_size}\n\n⚡️ ʜᴀᴘᴘʏ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴀɴᴅ ᴄᴏᴍᴇ ᴀɢᴀɪɴ 🌼")
+        return await message.reply(f"**<b>⚡️ ʜᴀᴘᴘʏ ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ᴀɴᴅ ᴄᴏᴍᴇ ᴀɢᴀɪɴ 🌼</b>**")
     files = files_[0]
     title = files.file_name
     size=get_size(files.file_size)
